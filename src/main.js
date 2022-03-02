501f0b32c36030eaa34e865534bfc97914bf2081
RamazanUstuntas <ustuntasramazan@outlook.com>
2022-03-03 00:49:48 +0300

    import scss and css files

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/scss/sb-admin-2.scss'
+import './assets/vendor/fontawesome-free/css/all.min.css'
 
 createApp(App).mount('#app')
